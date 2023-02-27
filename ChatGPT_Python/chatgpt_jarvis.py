bca3aa557a72b3f461dcb50f9fae9715807a6b00
Itthidej Aramsri <100425084+ObiOn777@users.noreply.github.com>
2023-02-27 23:49:21 +0700

    Update chatgpt_jarvis.py

--- ChatGPT_Python/chatgpt_jarvis.py
+++ ChatGPT_Python/chatgpt_jarvis.py
@@ -1,31 +1,36 @@
-import sys
 import subprocess
 import json
 import pyttsx3
+import speech_recognition as sr
 from colorama import *
 
-# Set the OpenAI API token
-#openai_api_key = os.getenv("CHATGPT_TOKEN")
 engine = pyttsx3.init()
-voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_enUS_ZiraDesktop"
-# Set the properties for the pyttsx3 engine
-engine.setProperty("voice", voice_id)
-engine.setProperty("rate", 180) # you can change the speaking rate to fit your preference
-# Get the prompt from the command line argument
-if len(sys.argv) < 2:
-    print("Please provide a prompt as a command line argument.")
-    sys.exit(1)
+r = sr.Recognizer()
+engine.setProperty('rate', 150)
 
-prompt = sys.argv[1]
+with sr.Microphone() as source:
+    print("Speak now")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said: ", text)
+    #engine.say(text)
+    engine.runAndWait()
+except sr.UnknownValueError:
+    print("Could not understand")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
+# Get the prompt from the command line argument
 
 # Set up the curl command
 curl_command = [
     "curl", "-s", "https://api.openai.com/v1/completions",
     "-H", "Content-Type: application/json",
-    "-H", f"Authorization: Bearer Chatgpt_api",
+    "-H", f"Authorization: Bearer ChatGPT_apikey",
     "-d", json.dumps({
         "model": "text-davinci-003",
-        "prompt": prompt,
+        "prompt": text,
         "max_tokens": 4000,
         "temperature": 1.0
     })
@@ -34,11 +39,12 @@ curl_command = [
 # Run the curl command and get the response
 output = subprocess.check_output(curl_command).decode("utf-8")
 
+# Parse the response with jq
 response = json.loads(output)
 generated_text = response["choices"][0]["text"]
 # Print the generated text
 
-print(Fore.RED + Style.BRIGHT + "\n[+] Input:", Fore.WHITE + prompt)
+print(Fore.RED + Style.BRIGHT + "\n[+] Input:", Fore.WHITE + text)
 print(Fore.GREEN + Style.BRIGHT + "\n[+] Output:", Fore.WHITE + generated_text)
 print("")
 
